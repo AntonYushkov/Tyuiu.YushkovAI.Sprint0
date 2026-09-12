48aa6b29489a262a1e6abeef547a8eced16dc395
AntonYushkov <anthonyyushckov@yandex.ru>
2026-09-13 02:15:09 +0500

    Task4

---
+++ Tyuiu.YushkovAI.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,7 @@
+namespace Tyuiu.YushkovAI.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        // Intentionally empty per task instructions
+    }
+}
---
+++ Tyuiu.YushkovAI.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Tyuiu.YushkovAI.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intentionally left blank per task instructions
+        }
+    }
+}
