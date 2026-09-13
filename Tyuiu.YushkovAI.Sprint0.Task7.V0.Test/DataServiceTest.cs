e5a9b27c78f3b90807b34e146d94b3a8aa027b49
AntonYushkov <anthonyyushckov@yandex.ru>
2026-09-13 23:04:26 +0500

    Task7 and debug

---
+++ Tyuiu.YushkovAI.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.YushkovAI.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.YushkovAI.Sprint0.Task7.V0.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void CheckAdditionArraysValid()
+    {
+        int[] nums1 = new int[] { 1, 1, 1, 1, 1 };
+        int[] nums2 = new int[] { 1, 1, 1, 1, 1 };
+        int[] expected = new int[] { 2, 2, 2, 2, 2 };
+
+        int[] result = DataService.AdditionArrays(nums1, nums2);
+
+        CollectionAssert.AreEqual(expected, result);
+    }
+}
---
+++ Tyuiu.YushkovAI.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.YushkovAI.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.YushkovAI.Sprint0.Task7.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+        int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Массив №1:");
+
+        for (int i = 0; i < arrayNums1.Length; i++)
+        {
+            Console.Write(arrayNums1[i] + " ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Массив №2:");
+
+        for (int i = 0; i < arrayNums2.Length; i++)
+        {
+            Console.Write(arrayNums2[i] + " ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        if (arrayNums1.Length == arrayNums2.Length)
+        {
+            int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+            Console.WriteLine("Результат сложения:");
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write(resultArray[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: массивы имеют разную длину.");
+        }
+    }
+}
